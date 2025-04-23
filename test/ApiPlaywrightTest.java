5d0aed26ba81d49a800c06c10d4829ee08c81107
Mohamad Nasser <mohamadnasser13@hotmail.com>
2025-04-23 12:34:47 +0300

    Add changes

--- test/ApiPlaywrightTest.java
+++ test/ApiPlaywrightTest.java
@@ -1,8 +1,6 @@
 import com.microsoft.playwright.*;
 import com.microsoft.playwright.options.RequestOptions;
-
 import org.junit.jupiter.api.*;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ApiPlaywrightTest {
@@ -12,8 +10,7 @@ public class ApiPlaywrightTest {
   @BeforeAll
   static void setup() {
     playwright = Playwright.create();
-    APIRequest requestContext = playwright.request();
-    request = requestContext.newContext(new APIRequest.NewContextOptions()
+    request = playwright.request().newContext(new APIRequest.NewContextOptions()
       .setBaseURL("https://jsonplaceholder.typicode.com"));
   }
 
@@ -27,20 +24,50 @@ public class ApiPlaywrightTest {
   void testGetPost() {
     APIResponse response = request.get("/posts/1");
     assertEquals(200, response.status());
-
-    String json = response.text();
-    assertTrue(json.contains("\"userId\": 1"));
-    assertTrue(json.contains("\"id\": 1"));
+    assertTrue(response.text().contains("\"userId\": 1"));
+    assertEquals("application/json; charset=utf-8", response.headers().get("content-type"));
   }
 
   @Test
   void testCreatePost() {
     APIResponse response = request.post("/posts", RequestOptions.create()
-      .setData("{\"title\": \"foo\", \"body\": \"bar\", \"userId\": 1}")
-      .setHeader("Content-Type", "application/json"));
+      .setHeader("Content-Type", "application/json")
+      .setData("{\"title\": \"foo\", \"body\": \"bar\", \"userId\": 1}"));
 
-    assertEquals(401, response.status());
-    String json = response.text();
-    assertTrue(json.contains("\"title\": \"foo\""));
+    assertEquals(201, response.status());
+    assertTrue(response.text().contains("\"title\": \"foo\""));
+  }
+
+  @Test
+  void testUpdatePost() {
+    APIResponse response = request.put("/posts/1", RequestOptions.create()
+      .setHeader("Content-Type", "application/json")
+      .setData("{\"id\": 1, \"title\": \"updated title\", \"body\": \"updated body\", \"userId\": 1}"));
+
+    assertEquals(200, response.status());
+    assertTrue(response.text().contains("\"title\": \"updated title\""));
+  }
+
+  @Test
+  void testDeletePost() {
+    APIResponse response = request.delete("/posts/1");
+    assertEquals(200, response.status());
+  }
+
+  @Test
+  void testError404() {
+    APIResponse response = request.get("/invalid-endpoint");
+    assertEquals(404, response.status());
+  }
+
+  @Test
+  void testTimeoutBehavior() {
+    APIRequestContext customRequest = playwright.request().newContext(new APIRequest.NewContextOptions()
+      .setBaseURL("https://jsonplaceholder.typicode.com")
+      .setTimeout(1000)); // 1 second timeout
+
+    APIResponse response = customRequest.get("/posts/1");
+    assertEquals(200, response.status());
+    customRequest.dispose();
   }
 }
