7720cda1480275d46a294e785dcb47023664ba95
Mohamad Nasser <mohamadnasser13@hotmail.com>
2025-04-24 13:28:22 +0300

    Add changes

--- test/UI_API_Test.java
+++ test/UI_API_Test.java
@@ -73,6 +73,8 @@ public class UI_API_Test {
 		page.getByRole(AriaRole.IMG, new Page.GetByRoleOptions().setName("menu")).click();
 		page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Sign out")).click();
 		
+		
+		
 		page.onResponse(response -> {
             if (response.url().contains("/api/signout")) {
                 // Check if the status code is 200
@@ -87,7 +89,7 @@ public class UI_API_Test {
                 // Check the message in the sign-out response
                 JSONObject jsonResponse = new JSONObject(bodyString);
                 String message = jsonResponse.getString("message");
-                Assertions.assertEquals("USER SIGNOUT SUCCESFULLY", message, "Sign-out message mismatch");
+                Assertions.assertEquals("USER SIGNOUT n SUCCESFULLY", message, "Sign-out message mismatch");
             }
         });
 		page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Enter Email")).click(new Locator.ClickOptions()
