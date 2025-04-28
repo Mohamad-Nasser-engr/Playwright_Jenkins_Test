e3dfebf6249fe7d944f1981eea75d95038fe6d7d
Mohamad Nasser <mohamadnasser13@hotmail.com>
2025-04-28 13:41:52 +0300

    Add changes

--- test/test_junit2.java
+++ test/test_junit2.java
@@ -11,8 +11,8 @@ public class test_junit2 {
   void test(Page page) {
     page.navigate("https://freelance-learn-automation.vercel.app/login");
     page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Enter Email")).click();
-    page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Enter Email")).fill("test123@gmail.com");
-    //page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Enter Email")).fill("test2@example.com");
+    //page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Enter Email")).fill("test123@gmail.com");
+    page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Enter Email")).fill("test2@example.com");
     page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Enter Password")).click();
     page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Enter Password")).fill("test123");
     page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Sign in")).click();
